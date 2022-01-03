be4c2bf8b1e28c68bd39fd7657f1482ac2e4242c
George Whitcher <george@georgewhitcher.com>
2022-01-03 14:05:34 -0500

    file size increase

--- system-config.php
+++ system-config.php
@@ -2,3 +2,6 @@
 define('BASE_URL', 'http://'.gethostname().'/RetroPie-Web-GUI');
 define('SITE_TITLE', 'RetroPie Web GUI');
 define('BASE_DIR', getcwd().'/roms');
+
+ini_set('upload_max_filesize', '9999M');
+ini_set('post_max_size', '9999M');
